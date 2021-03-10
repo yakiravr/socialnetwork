08a4170d54aa2b76eadc306573fbed918a0a7008
yakiravraham <yakiravr@gmail.com>
2021-03-10 20:52:50 +0100

    login with part 3

--- client/src/login.js
+++ client/src/login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "./axios";
+import { Link } from "react-router-dom";
 
 export default class Login extends React.Component {
     constructor() {
@@ -51,6 +52,9 @@ export default class Login extends React.Component {
                     placeholder="Password"
                     onChange={(e) => this.handleChange(e)}
                 />
+                <div id="login_in_egistration">
+                    <Link to="/verification">rest password</Link>
+                </div>
 
                 <button onClick={() => this.handleClick()}>Login!</button>
             </div>
